573089ebb9b0b1ff9e33f28e7708fbd5d8d31881
S M Armaan Hossain <armaanhossainirtaza7@gmail.com>
2023-04-27 12:29:18 +0600

    practice-2 added

---
+++ practice-2.js
@@ -0,0 +1,19 @@
+// তোমার কাছে: ৮০০০০ টাকার বেশি হলে তুমি mac কিনবে, ৬০ টাকার বেশি হলে gaming ল্যাপটপ কিনবে, ৪০ হাজার টাকার বেশি হলে lenovo yoga কম্পিউটার কিনবে , ২০ হাজার টাকার বেশি হলে পুরান ল্যাপটপ কিনবে। না হয় তুমি মোবাইল দিয়ে কাজ চালাবে। 
+
+
+var salary ='45000';
+
+
+if (salary >80000) {
+    console.log('Mama mac kinum mama macccccc... ')
+}
+
+else if (salary >60000) {
+    console.log("Gaming laptop hobe mama Gaming laptop....")
+}
+else if (salary >40000) {
+    console.log('kichu korar nai vai Lenovo computer neee...')
+}
+else {
+    console.log('Tor moto gorib er vai dhunia te nai .. phn dia kam chal')
+}
