f0e200285c2a8b0d5222eeaeb8b5b5ee1db8e388
S M Armaan Hossain <armaanhossainirtaza7@gmail.com>
2023-04-27 12:23:22 +0600

    Index of Number check

--- array.js
+++ array.js
@@ -4,3 +4,4 @@ console.log("length of Array =", numbers.length);
 console.log("Array type : ", typeof numbers);
 console.log("Is it an Array : ", Array.isArray(numbers));
 console.log("Number in index [3] = ", numbers[3]);
+console.log("Index of Number (7) = ", numbers.indexOf(7));
