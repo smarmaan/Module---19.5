4b4d79dc4dcd98e5f6c36769eecf62cf51d44412
S M Armaan Hossain <armaanhossainirtaza7@gmail.com>
2023-04-27 12:22:31 +0600

    Array type check

--- array.js
+++ array.js
@@ -2,3 +2,4 @@ let numbers = [2, 5, 7, 9, 6, 1];
 console.log("Array", numbers);
 console.log("length of Array =", numbers.length);
 console.log("Array type : ", typeof numbers);
+console.log("Is it an Array : ", Array.isArray(numbers));
